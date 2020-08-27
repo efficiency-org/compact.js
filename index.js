2f49c870ee3224073ff65e1654e1047361e51549
Dean Summer <deanlovesmargie@gmail.com>
2020-08-26 20:53:54 -0500

    Add module

---
+++ index.js
@@ -0,0 +1,20 @@
+module.exports = {
+	class Server {
+		constructor(function, port) {
+			try {
+				this.function = function;
+				this.port = port;
+
+				const http = require('http');
+				const server = http.createServer(this.function);
+
+				server.listen(this.port);
+
+				this.server = server;
+			} catch (error) {
+				console.error('Could not create server:', error);
+				process.exit(1);
+			}
+		}
+	}
+}
